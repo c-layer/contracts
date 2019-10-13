ce71e4d3a9896ad08eb01b09aa2f89654fb35765
Cyril Lapinte <cyril.lapinte@free.fr>
2019-10-13 11:12:11 +0200

    Updating truffle-config config

--- c-layer-core/truffle-config.js
+++ c-layer-core/truffle-config.js
@@ -76,8 +76,17 @@ module.exports = {
     // Useful for deploying to a public network.
     // NB: It's important to wrap the provider as a function.
     ropsten: {
-      provider: () => new HDWalletProvider(mnemonic, "https://ropsten.infura.io/v3/"+projectId),
+      provider: () => new HDWalletProvider(mnemonic, "https://ropsten.infura.io/v3/"+projectId, 0, 3),
       network_id: 3,       // Ropsten's id
+      gas: 5500000,        // Ropsten has a lower block limit than mainnet
+      confirmations: 2,    // # of confs to wait between deployments. (default: 0)
+      timeoutBlocks: 200,  // # of blocks before a deployment times out  (minimum/default: 50)
+      skipDryRun: true     // Skip dry run before migrations? (default: false for public nets )
+    },
+
+    mainnet: {
+      provider: () => new HDWalletProvider(mnemonic, "https://mainnet.infura.io/v3/"+projectId),
+      network_id: 1,       // Ropsten's id
       gas: 5500000,        // Ropsten has a lower block limit than mainnet
       confirmations: 2,    // # of confs to wait between deployments. (default: 0)
       timeoutBlocks: 200,  // # of blocks before a deployment times out  (minimum/default: 50)
