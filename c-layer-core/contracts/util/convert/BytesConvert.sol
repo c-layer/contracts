fd529fc5457be0cd6bd0b072608a24d77e5cb6d9
Cyril Lapinte <cyril.lapinte@free.fr>
2019-10-15 10:35:00 +0200

    Adding length requires when converting from bytes32

--- c-layer-core/contracts/util/convert/BytesConvert.sol
+++ c-layer-core/contracts/util/convert/BytesConvert.sol
@@ -6,26 +6,32 @@ pragma solidity >=0.5.0 <0.6.0;
  * @dev Convert bytes into different types
  *
  * @author Cyril Lapinte - <cyril.lapinte@openfiz.com>
- */
+ *
+ * Error Messages:
+ *   BC01: source must be a valid 32-bytes length
+ *   BC02: source must not be greater than 32-bytes
+ **/
 library BytesConvert {
 
   /**
   * @dev toUint256
   */
-  function toUint256(bytes memory source) internal pure returns (uint256 result) {
+  function toUint256(bytes memory _source) internal pure returns (uint256 result) {
+    require(_source.length == 32, "BC01");
     // solhint-disable-next-line no-inline-assembly
     assembly {
-      result := mload(add(source, 0x20))
+      result := mload(add(_source, 0x20))
     }
   }
 
   /**
   * @dev toBytes32
   */
-  function toBytes32(bytes memory source) internal pure returns (bytes32 result) {
+  function toBytes32(bytes memory _source) internal pure returns (bytes32 result) {
+    require(_source.length <= 32, "BC02");
     // solhint-disable-next-line no-inline-assembly
     assembly {
-      result := mload(add(source, 0x20))
+      result := mload(add(_source, 0x20))
     }
   }
 }
