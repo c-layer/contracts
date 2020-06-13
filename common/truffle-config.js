3e08638ed9296489bda8f9486c7d801567b67891
Cyril Lapinte <cyril.lapinte@free.fr>
2020-06-13 08:02:54 +0200

    Adding Kovan

--- common/truffle-config.js
+++ common/truffle-config.js
@@ -86,12 +86,19 @@ let config = {
     goerli: {
       provider: () => new HDWalletProvider(mnemonic, "https://goerli.infura.io/v3/"+projectId, 0, 5),
       network_id: 5,       // Goerli's id
-      gas: 5500000,        // Ropsten has a lower block limit than mainnet
+      gas: 5500000,        //
       confirmations: 2,    // # of confs to wait between deployments. (default: 0)
       timeoutBlocks: 200,  // # of blocks before a deployment times out  (minimum/default: 50)
       skipDryRun: true     // Skip dry run before migrations? (default: false for public nets )
     },
-
+    kovan: {
+      provider: () => new HDWalletProvider(mnemonic, "https://kovan.infura.io/v3/"+projectId, 0, 5),
+      network_id: 42,      // Kovan's id
+      gas: 5500000,        //
+      confirmations: 2,    // # of confs to wait between deployments. (default: 0)
+      timeoutBlocks: 200,  // # of blocks before a deployment times out  (minimum/default: 50)
+      skipDryRun: true     // Skip dry run before migrations? (default: false for public nets )
+    },
     mainnet: {
       provider: () => new HDWalletProvider(mnemonic, "https://mainnet.infura.io/v3/"+projectId, 0, 5),
       network_id: 1,       // Mainnet's id
