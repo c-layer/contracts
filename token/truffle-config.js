298bca869cd34ee47d29321a799af19c7a0ead05
Cyril Lapinte <cyril.lapinte@free.fr>
2020-06-13 23:06:09 +0200

    Updating truffle-config

--- token/truffle-config.js
+++ token/truffle-config.js
@@ -164,25 +164,28 @@ let config = {
   }
 }
 
-// Injecting endpoints
-Object.keys(endpoints).forEach((name) => {
-  if (!config.networks[name]) {
-    let template = {
-      network_id: 9999,       // Goerli's id
-      gas: 5500000,        // Goerli has a lower block limit than mainnet
-      confirmations: 2,    // # of confs to wait between deployments. (default: 0)
-      timeoutBlocks: 200,  // # of blocks before a deployment times out  (minimum/default: 50)
-      skipDryRun: true     // Skip dry run before migrations? (default: false for public nets )
-    };
-    Object.keys(config.networks).forEach((existingNetworkName) => {
-      if (name.startsWith(existingNetworkName)) {
-        template = config.networks[existingNetworkName];
-      }
-    });
-    template.provider = 
-      () => new HDWalletProvider(mnemonic, endpoints[name], 0, 5),
-    config.networks[name] = template;
-  }
-});
+if (endpoints) {
+
+  // Injecting endpoints
+  Object.keys(endpoints).forEach((name) => {
+    if (!config.networks[name]) {
+      let template = {
+        network_id: 9999,       // Goerli's id
+        gas: 5500000,        // Goerli has a lower block limit than mainnet
+        confirmations: 2,    // # of confs to wait between deployments. (default: 0)
+        timeoutBlocks: 200,  // # of blocks before a deployment times out  (minimum/default: 50)
+        skipDryRun: true     // Skip dry run before migrations? (default: false for public nets )
+      };
+      Object.keys(config.networks).forEach((existingNetworkName) => {
+        if (name.startsWith(existingNetworkName)) {
+          template = config.networks[existingNetworkName];
+        }
+      });
+      template.provider = 
+        () => new HDWalletProvider(mnemonic, endpoints[name], 0, 5),
+      config.networks[name] = template;
+    }
+  });
+}
 
 module.exports = config;
