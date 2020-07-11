908f03d6396fbce00c200093aab0d5ecf4006dc0
Bertrand Juglas <b@juglas.name>
2020-07-11 20:14:50 +0200

    detect if sudo is needed or not

--- start.sh
+++ start.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
 
+set -e
+if [[ `groups | grep -c "\bdocker\b"` -eq 1 ]]; then
+        SUDO=""
+else
+        SUDO=sudo
+fi
+
+echo $SUDO
 pwd
-docker run -it \
+$SUDO docker run -it \
 	-p 33000:3000 -p 33001:3001 -p 8080:8080 \
         -p 8545:8545 -p 9545:9545 \
         -v $PWD:/home/node/project \
