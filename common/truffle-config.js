9701f6707728ede14229dd66c0862327b27a49db
Cyril Lapinte <cyril.lapinte@free.fr>
2020-09-18 11:34:51 +0200

    Adding rinkeby in truffle-config

--- common/truffle-config.js
+++ common/truffle-config.js
@@ -84,6 +84,14 @@ let config = {
       timeoutBlocks: 200,  // # of blocks before a deployment times out  (minimum/default: 50)
       skipDryRun: true     // Skip dry run before migrations? (default: false for public nets )
     },
+    rinkeby: {
+      provider: () => new HDWalletProvider(mnemonic, "https://rinkeby.infura.io/v3/"+projectId, 0, 5),
+      network_id: 4,       // Goerli's id
+      gas: 5500000,        //
+      confirmations: 2,    // # of confs to wait between deployments. (default: 0)
+      timeoutBlocks: 200,  // # of blocks before a deployment times out  (minimum/default: 50)
+      skipDryRun: true     // Skip dry run before migrations? (default: false for public nets )
+    },
     goerli: {
       provider: () => new HDWalletProvider(mnemonic, "https://goerli.infura.io/v3/"+projectId, 0, 5),
       network_id: 5,       // Goerli's id
