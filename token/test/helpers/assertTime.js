264860d9514f83f31389d574e408f699883ca8af
Cyril Lapinte <cyril.lapinte@free.fr>
2021-02-05 17:58:44 +0100

    Linting on assertTime

--- token/test/helpers/assertTime.js
+++ token/test/helpers/assertTime.js
@@ -3,12 +3,12 @@
  * @author Cyril Lapinte - <cyril@openfiz.com>
  */
 
-const MAX_TOLERANCE_VALUE = 1; //secondes
+const MAX_TOLERANCE_VALUE = 1; // in secondes
 
 module.exports = function (actualTime, expectedTime, message) {
-  if (actualTime != expectedTime) {
-    const delta = (actualTime > expectedTime) ?
-      actualTime - expectedTime : expectedTime - actualTime;
+  if (actualTime !== expectedTime) {
+    const delta = (actualTime > expectedTime)
+      ? actualTime - expectedTime : expectedTime - actualTime;
 
     if (delta > MAX_TOLERANCE_VALUE) {
       assert.equal(actualTime, expectedTime, message);
