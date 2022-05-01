6cce9a9ba17af0a31e18c15b41479ec87f123eb2
Cyril <cyril.lapinte@free.fr>
2022-05-01 18:49:00 +0000

    Reworking mintInternal in ERC721 to support multiple minting operations

--- distribution/contracts/interface/IVault.sol
+++ distribution/contracts/interface/IVault.sol
@@ -1,29 +1,17 @@
 pragma solidity ^0.8.0;
 
-import "@c-layer/common/contracts/interface/IERC20.sol";
+import "./IVaultETH.sol";
+import "./IVaultERC20.sol";
+import "./IVaultERC721.sol";
 
 
 /**
- * @title Vault
- * @dev Vault managing ERC20
+ * @title IVault
+ * @dev Vault managing ETH, ERC20 and ERC721
  * @author Cyril Lapinte - <cyril@openfiz.com>
  * SPDX-License-Identifier: MIT
  *
  * Error messages
  */
-abstract contract IVault {
-
-  function transfer(address payable _to, uint256 _value, bytes memory _data)
-    public virtual returns (bool, bytes memory);
-  function transferERC20(IERC20 _token, address _to, uint256 _value)
-    public virtual returns (bool);
-  function transferERC20From(
-    IERC20 _token, address _from, address _to, uint256 _value)
-    public virtual returns (bool);
-  function approve(IERC20 _token, address _spender, uint256 _value)
-    public virtual returns (bool);
-  function increaseApproval(IERC20 _token, address _spender, uint _addedValue)
-    public virtual returns (bool);
-  function decreaseApproval(IERC20 _token, address _spender, uint _subtractedValue)
-    public virtual returns (bool);
+abstract contract IVault is IVaultETH, IVaultERC20, IVaultERC721 {
 }
